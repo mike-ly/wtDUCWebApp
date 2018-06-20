fedae51af5fbe6b4f7ebe8c16e46b20b3826c8e9
mikelyxd <michael.ly.1998@gmail.com>
2018-06-19 20:32:48 -0700

    stuff

--- my-appls/src/components/dailyMenus.js
+++ my-appls/src/components/dailyMenus.js
@@ -9,7 +9,8 @@ class dailyMenus extends React.Component {
       meals: ["Breakfast", "Lunch", "Dinner"],
       generalMenu: [['Scrambled Tofu', 'Scrambled Eggs', 'Bacon', 'Turkey Bacon', 'Chicken Sausage', 'Pork Sausage', 'Turkey Sausage', 'Vegan Sausage Link', 'Biscuit', 'Hash Browns', 'Breakfast Potatoes', 'Tater Tots', 'Buttermilk Pancakes', 'Texas French Toast', 'Low Fat Fruit Yogurt', 'Lowfat Vanilla Yogurt', 'Plain Greek Yogurt', 'Cottage Cheese', 'Belgian Waffle', 'Pancake Syrup'],
                     ['Cheese Pizza', 'Pepperoni Pizza', 'Vegetable Pizza', 'Grilled Springer Mountain Farms Chicken Breast', 'Roasted Vegetables', 'Roasted Portobello Mushroom', 'White Oak Pastures Grass-fed Beef Patty', 'All-beef Hot Dog', 'Sweet Potato Fries', 'French Fries', 'Creamy Mashed Potatoes', 'House-made Potato Chips', 'Hard-boiled Egg (1 Each)', 'Tofu', 'Chicken Breast', 'Tuna'],
-                    ['Cheese Pizza', 'Pepperoni Pizza', 'Vegetable Pizza', 'Grilled Springer Mountain Farms Chicken Breast', 'Roasted Vegetables', 'Roasted Portobello Mushroom', 'White Oak Pastures Grass-fed Beef Patty', 'All-beef Hot Dog', 'Sweet Potato Fries', 'French Fries', 'Creamy Mashed Potatoes', 'House-made Potato Chips', 'Hard-boiled Egg (1 Each)', 'Tofu', 'Chicken Breast', 'Tuna']]
+                    ['Cheese Pizza', 'Pepperoni Pizza', 'Vegetable Pizza', 'Grilled Springer Mountain Farms Chicken Breast', 'Roasted Vegetables', 'Roasted Portobello Mushroom', 'White Oak Pastures Grass-fed Beef Patty', 'All-beef Hot Dog', 'Sweet Potato Fries', 'French Fries', 'Creamy Mashed Potatoes', 'House-made Potato Chips', 'Hard-boiled Egg (1 Each)', 'Tofu', 'Chicken Breast', 'Tuna']],
+      generalMenuScore: [[],[],[]]
     }
   }
 
@@ -27,7 +28,7 @@ class dailyMenus extends React.Component {
     let menuItems = [];
     const relevantMenu = this.state.generalMenu[index].slice();
     for (let i = 0; i < relevantMenu.length; i++) {
-      menuItems.push(this.renderMenuItem(props, relevantMenu[i]));
+      menuItems.push(this.renderMenuItem(props, relevantMenu[i], index, i));
     }
     return (
       <DailyMenu
@@ -39,10 +40,16 @@ class dailyMenus extends React.Component {
     );
   }
 
-  renderMenuItem(props, item) {
+  renderMenuItem(props, item, menu_index, item_index) {
+    var score;
     const numbers = ["one", "two", "three", "four", "five"];
-    const score = Math.trunc(Math.random() * 5);
-    const classes = "standardText menuItemText " + numbers[score];
+    if (this.state.generalMenuScore[menu_index].length != this.state.generalMenu[menu_index].length) {
+      score = Math.trunc(Math.random() * 5);
+      this.state.generalMenuScore[menu_index].push(score);
+    } else {
+      score = this.state.generalMenuScore[menu_index][item_index];
+    }
+    var classes = "standardText menuItemText " + numbers[score];
     return (
       <p className={classes}>{item}</p>
     );
